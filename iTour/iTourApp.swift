25cfc9336abacb316958fff7c7621c917c262732
Jordon Bigelow <the-music-dude@hotmail.com>
2024-06-18 11:04:52 -0600

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Jordon Bigelow on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
