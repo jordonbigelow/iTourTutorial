aa6a2e00fb0ba30511fe626a47196560d0e351df
Jordon Bigelow <the-music-dude@hotmail.com>
2024-06-18 11:53:51 -0600

    initial commit

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query var destinations: [Destination]
+    @Environment(\.modelContext) var modelContext
+    @State private var path = [Destination]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List {
+                ForEach(destinations) { destintation in
+                    NavigationLink(value: destintation) {
+                        VStack(alignment: .leading) {
+                            Text(destintation.name)
+                                .font(.headline)
+                            
+                            Text(destintation.date.formatted(date: .long, time: .shortened))
+                        }
+                    }
+                }
+                .onDelete(perform: deleteDesinations)
+            }
+            .navigationTitle("iTour")
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+            .toolbar {
+                Button("Add Destination", systemImage: "plus", action: addDestination)
+            }
         }
-        .padding()
+    }
+    
+    func deleteDesinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
+    }
+    
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
     }
 }
 
